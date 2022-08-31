a307bb014c4847e97f70ecfb3e525de2fe92e73b
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-08-31 14:53:22 -0700

    add comment model and routes, add getters for dateFormat

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,20 @@ const pizzaController = {
   // get all pizzas - GET /api/pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      //to show commentContent Need To populate a field,
+      //chain the .populate() method onto your query, passing in an object with the key
+      // path plus the value of the field you want populated
+      .populate({
+        path: "comments",
+        //used the select option inside of populate(), to tell Mongoose we dont want __v field
+        //on comments either. The minus sign - indicates that we don't want it to be returned.
+        //If we didn't have it, it would return only the __v field.
+        select: "-__v",
+      })
+      .select("-__v")
+      //use .sort({ _id: -1 }) to sort in DESC order by the _id value. This gets the newest
+      //pizza because a timestamp value is hidden somewhere inside the MongoDB ObjectId.
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -14,6 +28,11 @@ const pizzaController = {
   // get one pizza by id - GET /api/pizzas/:id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
         // If no pizza is found, send 404
         if (!dbPizzaData) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,28 +1,54 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-//MongoDB provides _id so  you don't have to set it up like you would in SQL. 
+//MongoDB provides _id so  you don't have to set it up like you would in SQL.
 //If you wanted to change the name of the _id field— to pizzaId,
 //you could override it in Mongoose.
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
     },
     createdBy: {
-        type: String
+      type: String,
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
+      //getters to transform data using utils function dateFormat
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-        type: String,
-        default: 'Large'
+      type: String,
+      default: "Large",
     },
-    toppings: []
+    toppings: [],
+    //creating relationship to comment model
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  //tell schema that it can use virturals
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    //set id to false because this is a virtual that Mongoose returns, and we don’t need it
+    id: false,
+  }
+);
+
+// Get total count of comments and replies on retrieval (using Virtuals)
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 //create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 //export the Pizza model
 module.exports = Pizza;
